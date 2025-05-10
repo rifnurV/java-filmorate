c239cde75e5fe94c4c8f8906fea373cde500b5d6
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-05-10 23:14:25 +0300

    Film обавил @Data удалил @Getter @Setter

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,17 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import ru.yandex.practicum.filmorate.annotation.After1895ReleaseDate;
 
 import java.time.LocalDate;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+@Data
 public class Film {
     private Long id;
     @NotBlank(message = "Название не может быть пустым")
